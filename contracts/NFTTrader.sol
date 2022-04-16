37a5c5caf1dac821fa01eeedd666abf70de646e0
RaphaelNdonga <nkibi53@gmail.com>
2022-04-16 15:14:13 +0300

    nft trader that sends nfts added

---
+++ contracts/NFTTrader.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract NFTTrader is ERC721Holder {
+    constructor() {}
+
+    function sendNFT(
+        address _receiver,
+        address _token,
+        uint256 _tokenId
+    ) public {
+        IERC721(_token).safeTransferFrom(address(this), _receiver, _tokenId);
+    }
+}
