1605a881e0c357d7692766b7e30d13cee3fc1631
RaphaelNdonga <nkibi53@gmail.com>
2022-04-18 22:06:43 +0300

    testing whether I can correctly send and receive nfts through the nfttrader contract and my nft

---
+++ test/trader-test.js
@@ -0,0 +1,39 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFT Trader", function () {
+    this.timeout(50000);
+
+    let nftTraderContract;
+    let myNFTContract;
+    let owner;
+    let acc1;
+    let acc2;
+    this.beforeEach(async function () {
+        let nftTraderFactory = await ethers.getContractFactory("NFTTrader");
+        let myNFTFactory = await ethers.getContractFactory("MyNFT");
+        [owner, acc1, acc2] = await ethers.getSigners();
+
+        nftTraderContract = await nftTraderFactory.deploy();
+        myNFTContract = await myNFTFactory.deploy();
+
+        await nftTraderContract.deployed();
+        await myNFTContract.deployed();
+
+        await myNFTContract.safeMint(owner.address, "example.com")
+    });
+
+    it("Should receive an nft properly", async function () {
+        let txn = await myNFTContract.transferFrom(owner.address, acc1.address, 0);
+        await txn.wait();
+        expect(await myNFTContract.ownerOf(0)).to.equal(acc1.address);
+    });
+
+    it("Should send nft properly", async function () {
+        let txn = await myNFTContract.transferFrom(owner.address, nftTraderContract.address, 0);
+        await txn.wait();
+        txn = await nftTraderContract.sendNFT(acc2.address, myNFTContract.address, 0);
+        await txn.wait();
+        expect(await myNFTContract.ownerOf(0)).to.equal(acc2.address);
+    })
+})
