089ee13a26e2c6c3f4dc7d331bbf0ba4d496e484
RaphaelNdonga <nkibi53@gmail.com>
2022-04-20 19:39:58 +0300

    documented better

--- contracts/NFTTrader.sol
+++ contracts/NFTTrader.sol
@@ -6,7 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-//ERC721Holder enables smart contracts to be able to possess nfts.
+/*
+ERC721Holder enables smart contracts to be able to possess nfts.
+*/
 contract NFTTrader is ERC721Holder {
     mapping(uint256 => address) tokenIdAddressMap;
     address private cUsdTokenAddress;
@@ -15,17 +17,31 @@ contract NFTTrader is ERC721Holder {
         cUsdTokenAddress = _cUsdTokenAddress;
     }
 
+    /*
+    When someone wants to purchase an NFT, this function runs to 'sell' it to them.
+    */
     function sellNFT(
         address _receiver,
         address _token,
         uint256 _tokenId,
         uint256 _price
     ) public {
+        //get the address that was stored by the front end when the seller wanted to 'sell'
         address seller = getTokenOwner(_tokenId);
+
+        //Transfer cUsd tokens to the original seller of the nft.
         IERC20(cUsdTokenAddress).transfer(seller, _price);
+
+        //Remove the 'seller' data because they have been compensated.
         removeUserData(_tokenId);
+        //Transfer the nft to the interested buyer
         IERC721(_token).safeTransferFrom(address(this), _receiver, _tokenId);
     }
+
+    /**
+    This function runs when the seller wants to sell the nft. It stores their address
+    together with the token id of the token they wish to sell
+     */
 
     function storeUserData(address _user, uint256 _tokenId) public {
         tokenIdAddressMap[_tokenId] = _user;
